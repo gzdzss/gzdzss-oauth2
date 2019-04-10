f3fb4aeb06b81122fc701293260a1506350852a6
zyj <zhouyanjie@wtoip.com>
2019-04-10 11:19:16 +0800

    添加一个 webflux demo

--- gzdzss-gateway/src/main/java/com/gzdzss/gateway/GatewayApplication.java
+++ gzdzss-gateway/src/main/java/com/gzdzss/gateway/GatewayApplication.java
@@ -3,6 +3,13 @@ package com.gzdzss.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 /**
  * @author <a href="mailto:zhouyanjie666666@gmail.com">zyj</a>
@@ -13,6 +20,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class GatewayApplication {
 
+    // webflux
+    @Bean
+    public RouterFunction<ServerResponse> index() {
+        return route(GET("/"), request -> {
+            String hostAddress = request.exchange().getRequest().getRemoteAddress().getAddress().getHostAddress();
+            return ServerResponse.ok().body(Mono.just(hostAddress), String.class);
+        });
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
