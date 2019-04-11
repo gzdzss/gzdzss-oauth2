dcd038a310af20ce983f4760a976708d9c7e386b
zyj <zhouyanjie@wtoip.com>
2019-04-11 10:07:23 +0800

    token 过期提示

--- gzdzss-gateway/src/main/java/com/gzdzss/gateway/web/filter/AuthFilter.java
+++ gzdzss-gateway/src/main/java/com/gzdzss/gateway/web/filter/AuthFilter.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author <a href="mailto:zhouyanjie666666@gmail.com">zyj</a>
@@ -21,19 +27,18 @@ public class AuthFilter implements GlobalFilter, Ordered {
 
     private static final String JWT_TO_ACCESS = "jwt_to_access:";
 
-    //是否拦截  accessToken 不在redis中的请求
-    private static final boolean INTERCEPT_BEARER = false;
+    private static final byte[] TOKEN_TIMEOUT_MSG= "token已过期".getBytes(StandardCharsets.UTF_8);
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
         HttpHeaders headers = exchange.getRequest().getHeaders();
         String token = headers.getFirst(HttpHeaders.AUTHORIZATION);
-        if (token != null && token.startsWith("Bearer ")) {
+        if (token != null && token.toUpperCase().startsWith("Bearer ".toUpperCase())) {
             String accessToken = token.substring(7);
+            //根据 accessToken 获取 jwtToken, 如果没有，则表示已经过期
             String jwtToken = stringRedisTemplate.opsForValue().get(JWT_TO_ACCESS + accessToken);
             if (jwtToken != null) {
                 ServerHttpRequest host = exchange.getRequest().mutate().headers(httpHeaders -> {
@@ -41,12 +46,14 @@ public class AuthFilter implements GlobalFilter, Ordered {
                 }).build();
                 ServerWebExchange build = exchange.mutate().request(host).build();
                 return chain.filter(build);
+            } else {
+                //过期提示:  statusCode: 401 ,  body:  token已过期
+                ServerHttpResponse response = exchange.getResponse();
+                DataBuffer buffer = response.bufferFactory().wrap(TOKEN_TIMEOUT_MSG);
+                response.getHeaders().setContentType(MediaType.APPLICATION_JSON_UTF8);
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.writeWith(Mono.just(buffer));
             }
-
-            if (INTERCEPT_BEARER) {
-                throw new RuntimeException("token已过期");
-            }
-
         }
         return chain.filter(exchange);
     }
